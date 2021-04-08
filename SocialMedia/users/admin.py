71ebfe221541770db122ad6f6fbb89c847d47ce3
Shane <>
2021-04-08 13:15:20 -0400

    customized admin pannel for User: displays more info

--- SocialMedia/users/admin.py
+++ SocialMedia/users/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+class AdminConfig(admin.ModelAdmin):
+    exclude = ('password',)
+    list_display = ('username', 'date_joined', 'is_staff', 'is_superuser')
+
+admin.site.register(User, AdminConfig)
