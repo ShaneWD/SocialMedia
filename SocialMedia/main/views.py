007af141d49f7f723d0d914653550024b2cbf8ba
Shane <>
2021-04-13 14:59:46 -0400

    added login_required decorator

--- SocialMedia/main/views.py
+++ SocialMedia/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def home(request):
     return HttpResponse("<h1>Hello<h1>")
