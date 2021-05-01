07b4c20b460a0ae27bf7e42e14951c24632a3a85
Shane <>
2021-05-01 12:06:25 -0400

    fixed bugs; actually goes to /chat/lobby/

--- SocialMedia/chat/urls.py
+++ SocialMedia/chat/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.chat_home, name = "home"),
+    path('<str:room_name>/', views.room, name="room"),
 ]
--- SocialMedia/chat/views.py
+++ SocialMedia/chat/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def chat_home(request):
+    
     context = {
 
     }
     return render(request, "chat/chat_home.html", context)
+
+
+def room(request, room_name):
+
+    context = {
+        'room_name': room_name
+    }
+    return render(request, 'chat/chat_room.html', context)
