78a11f52d221443edd150a90173123f19c3ec4bb
Shane <>
2021-05-01 11:53:40 -0400

    integrated channels

---
+++ SocialMedia/asgi.py
@@ -0,0 +1,12 @@
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+  "http": AsgiHandler(),
+})
